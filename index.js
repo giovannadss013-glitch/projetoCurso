9096a62e78a35bf653b826baf2d9b238e0c4c71f
giovannadss013 <giovannadss013@gmail.com>
2026-03-03 00:34:37 -0300

    Projeto jornada do heroi - desafio iniciante

--- index.js
+++ index.js
@@ -1,54 +1,9 @@
-// Função que calcula o rank do herói
-function calcularRank(vitorias, derrotas) {
-    
-    let saldoVitorias = vitorias - derrotas
-    let nivel = ""
+// Lendo os valores de entrada
+let posicaoInicial = parseInt(gets());
+let totalPassos = parseInt(gets());
 
-    // Estrutura de decisão
-    if (vitorias <= 10) {
-        nivel = "Ferro"
-    } 
-    else if (vitorias <= 20) {
-        nivel = "Bronze"
-    } 
-    else if (vitorias <= 50) {
-        nivel = "Prata"
-    } 
-    else if (vitorias <= 80) {
-        nivel = "Ouro"
-    } 
-    else if (vitorias <= 90) {
-        nivel = "Diamante"
-    } 
-    else if (vitorias <= 100) {
-        nivel = "Lendário"
-    } 
-    else {
-        nivel = "Imortal"
-    }
+// Calculando a posição final
+let posicaoFinal = posicaoInicial + totalPassos;
 
-    return { saldoVitorias, nivel }
-}
-
-// Lista de heróis identificados
-let jogadores = [
-    { nome: "Harron", vitorias: 5, derrotas: 2 },
-    { nome: "Orion", vitorias: 25, derrotas: 10 },
-    { nome: "Lian", vitorias: 75, derrotas: 20 },
-    { nome: "Gohan", vitorias: 105, derrotas: 30 }
-]
-
-// Laço de repetição
-for (let i = 0; i < jogadores.length; i++) {
-
-    let resultado = calcularRank(
-        jogadores[i].vitorias,
-        jogadores[i].derrotas
-    )
-
-    console.log(
-        "O herói " + jogadores[i].nome +
-        " tem saldo de " + resultado.saldoVitorias +
-        " e está no nível de " + resultado.nivel
-    )
-}
+// Exibindo o resultado
+print("Posicao final do heroi: " + posicaoFinal);
